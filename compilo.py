84ec923a4a82d7bc52f6da98a3fa24b00ca032b3
Serp <manu.serpette74@gmail.com>
2022-05-25 09:11:15 +0200

    ==

--- compilo.py
+++ compilo.py
@@ -59,6 +59,7 @@ def pp_prg(prog):
     ret = pp_expr(prog.children[2])
     return f"main ({vars}) {{\n {bloc} \n return({ret});\n}}"
 
+
 def var_list(ast):
     if isinstance(ast, lark.Token):
         if ast.type == "IDENTIFIANT":
@@ -71,35 +72,36 @@ def var_list(ast):
     return s
 
 
+nb_while = 0
+nb_if = 0
 
-
-nb_while=0
-nb_if=0
 
 def compile_expr(expr):
     if expr.data == "binexpr":
         e1 = compile_expr(expr.children[0])
         e2 = compile_expr(expr.children[2])
         op = expr.children[1].value
-        if op=="+":
+        if op == "+":
             return f"{e2}\npush rax\n{e1}\npop rbx\nadd rax,rbx"
-        if op=="-":
+        if op == "-":
             return f"{e2}\npush rax\n{e1}\npop rbx\nsub rax,rbx"
-        if op=="*":
+        if op == "*":
             return f"{e2}\npush rax\n{e1}\npop rbx\nmul rax,rbx"
-        if op=="!=":
+        if op == "!=":
             return f"{e2}\npush rax\n{e1}\npop rbx\nsub rax,rbx"
-        if op=="==":
-            return f"{e2}\npush rax\n{e1}\npop rbx\nsub rax,rbx"
+        if op == "==":
+            return f"{e2}\npush rax\n{e1}\npop rbx\nsub rax,rbx\ncmp rax,0\nje finrax\nmov rax 1\njmp finrax\nfin:mov rax, 0\n"
+
     if expr.data == "parenexpr":
         return compile_expr(expr.children[0])
-    if expr.data =="variable":
-        e=expr.children[0].value
+    if expr.data == "variable":
+        e = expr.children[0].value
         return f"\nmov rax,[{e}]"
     if expr.data == "nombre":
-        e=expr.children[0].value
+        e = expr.children[0].value
         return f"\nmov rax,{e}"
-        
+
+
 def compile_cmd(cmd):
     global nb_while
     global nb_if
@@ -110,15 +112,16 @@ def compile_cmd(cmd):
     if cmd.data == "printf":
         return f"{compile_expr(cmd.children[0])}\nmov rdi,fmt\nmov rsi,rax\nxor rax,rax\ncall printf"
     if cmd.data == "if":
-        nb_if+=1
+        nb_if += 1
         e = compile_expr(cmd.children[0])
         b = compile_bloc(cmd.children[1])
         return f"{e}\ncmp rax,0\njz end_if{nb_if}\n{b}\nend_if{nb_if}:"
     if cmd.data == "while":
-        nb_while+=1
+        nb_while += 1
         e = compile_expr(cmd.children[0])
         b = compile_bloc(cmd.children[1])
         return f"\ndeb_while{nb_while}:\n{e}\ncmp rax,0\njz end_while{nb_while}\n{b}\njmp deb_while{nb_while}\nend_while{nb_while}:"
+
 
 def compile_bloc(bloc):
     res = ""
@@ -126,28 +129,30 @@ def compile_bloc(bloc):
         res += compile_cmd(cmd)
     return res
 
+
 def compile_prg(prog):
     vars = prog.children[0]
-    bloc =prog.children[1]
+    bloc = prog.children[1]
     ret = prog.children[2]
     return compile_bloc(bloc)
 
+
 def compile_vars(ast):
-    s=""
+    s = ""
     for i in range(len(ast.children)):
-        s+= f"\nmov rbx, [rbp-0x10]\nmov rdi,[rbx-{8*(i)}]\ncall atoi\nmov [{ast.children[i].value}],rax"
+        s += f"\nmov rbx, [rbp-0x10]\nmov rdi,[rbx-{8*(i)}]\ncall atoi\nmov [{ast.children[i].value}],rax"
     return s
 
 
 def compile(prg):
     with open("moule.asm") as f:
         code = f.read()
-        vars_decl="\n".join([f"{x}: dq 0" for x in var_list(prg)])
-        prog=compile_prg(prg)
-        code=code.replace("VAR_DECL",vars_decl)
-        code=code.replace("RETURN",compile_expr(prg.children[2]))
-        code=code.replace("BODY",prog)
-        code=code.replace("VAR_INIT",compile_vars(prg.children[0]))
+        vars_decl = "\n".join([f"{x}: dq 0" for x in var_list(prg)])
+        prog = compile_prg(prg)
+        code = code.replace("VAR_DECL", vars_decl)
+        code = code.replace("RETURN", compile_expr(prg.children[2]))
+        code = code.replace("BODY", prog)
+        code = code.replace("VAR_INIT", compile_vars(prg.children[0]))
         return code
 
 # print(compile_prg(grammaire.parse(program)))
@@ -156,5 +161,5 @@ def compile(prg):
 program = "main(a){a=10;while(a!=8){a=a-1;printf(a);}return(a);}"
 print(pp_prg(grammaire.parse(program)))
 print("\n")
-with open("prog.asm","w") as f:
+with open("prog.asm", "w") as f:
     f.write(compile(grammaire.parse(program)))
