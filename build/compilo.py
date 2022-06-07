1f56770f71a76bdd0e280609139174ebbeb6e5fa
DatBen <benjamindumont@live.com>
2022-06-07 20:41:45 +0200

    commite

--- build/compilo.py
+++ build/compilo.py
@@ -185,7 +185,7 @@ def compile_expr(expr):
     if expr.data == "float":
         e = expr.children[0].value
         return f"\nmovsd xmm0,[{float_dict[e]}]"
-    if expr.data == "new_array":
+    if expr.data == "new_array" or expr.data=="new_array_float":
         e = compile_expr(expr.children[0])
         res = f"{e}\npush rax\npop rbx\nimul rbx,8\nadd rbx,8\nmov rdi, rbx\ncall malloc\npush rax\n{e}\npop rbx\npush rax\nmov rax,rbx\npop rbx\nmov [rax], rbx\n"
         return res
@@ -205,6 +205,8 @@ def compile_cmd(cmd):
     global nb_while
     global nb_if
     if cmd.data == "assignement":
+        print(cmd.children[1])
+
         lhs = cmd.children[0].value
         rhs = compile_expr(cmd.children[1])
         if "xmm0" in rhs:
@@ -213,6 +215,7 @@ def compile_cmd(cmd):
             return f"{rhs}\nmovsd [{lhs}],xmm0"
         else:
             return f"{rhs}\nmov [{lhs}],rax"
+            
     if cmd.data == "printf":
         rhs = compile_expr(cmd.children[0])
         if (cmd.children[0].data == "variable") and cmd.children[0].children[0].value in floats:
@@ -239,7 +242,14 @@ def compile_cmd(cmd):
         lhs = cmd.children[0].value
         e = compile_expr(cmd.children[1])
         rhs = compile_expr(cmd.children[2])
-        return f"{e}\npush rax\nmov rax, [{lhs}]\npop rbx\nimul rbx,8\nadd rbx,8\nadd rax,rbx\npush rax\n{rhs}\npop rbx\nmov [rbx],rax"
+        if "xmm0" in rhs:
+            if lhs not in floats:
+                floats.append(lhs)
+            return f"{e}\npush rax\nmov rax, [{lhs}]\npop rbx\nimul rbx,8\nadd rbx,8\nadd rax,rbx\npush rax\n{rhs}\npop rbx\nmovsd [{lhs}],xmm0"
+        else:
+            return f"{e}\npush rax\nmov rax, [{lhs}]\npop rbx\nimul rbx,8\nadd rbx,8\nadd rax,rbx\npush rax\n{rhs}\npop rbx\nmov [rbx],rax"
+
+        
 
 
 def compile_bloc(bloc):
@@ -285,6 +295,8 @@ program = grammaire.parse(program)
 program = pp_prg(program)
 program = grammaire.parse(program)
 
+
+tab_float=[]
 float_dict = {}
 i = 0
 for f in float_list(program):
@@ -292,7 +304,11 @@ for f in float_list(program):
     i += 1
 
 
+
 print(pp_prg(program))
 print("\n")
 with open("build/prog.asm", "w") as f:
     f.write(compile(program))
+
+print(floats)
+
